69a734cd749ac4066ff7c9b496fac6526be6bec1
Zoe Chen <zoec58@nycstudents.net>
2022-09-30 09:29:23 -0400

    Added RectangleRunner, Rectangle, and ChatBot.

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(5.0);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //I'm predicting this will print the area of the circle.
+
+        // D. run the code and confirm your prediction! Were you correct?
+        //yes
+
+    }
 }
